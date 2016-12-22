3f1f5927019e29c00559b789d849b5472b24d71a
Xerotico <2008_96@mail.ru>
2016-12-22 13:09:42 +0300

    add routing and menu

--- js/app.js
+++ js/app.js
@@ -9,9 +9,19 @@
 				controller  : 'usersController'
 			})
 
+			.when('/users/:username', {
+				templateUrl : 'pages/user.html',
+				controller  : 'userController'
+			})
+
 			.when('/groups', {
 				templateUrl : 'pages/groups.html',
 				controller  : 'groupsController'
+			})
+
+			.when('/groups/:groupname', {
+				templateUrl : 'pages/group.html',
+				controller  : 'groupController'
 			})
 	});
 
@@ -19,11 +29,72 @@
 		this.message = 'Awesome main page!';
 	});
 
-	app.controller('usersController', function() {
-		this.message = 'Awesome users information!';
+	app.controller('navController', function() {
+		this.tabs = [
+			{ "label":"Главная", "href":"#" },
+			{ "label":"Группы", "href":"#groups" },
+			{ "label":"Пользователи", "href":"#users" }
+		];
+
+		this.activeTab; // It should be = this.tabs[0]? But if we refresh page - first tab is selected as default. 
+
+		this.setActiveTab = function(selectedTab){
+			this.activeTab = selectedTab;
+		};
 	});
 
-	app.controller('groupsController', function() {
-		this.message = 'Awesome groups information!';
+	app.controller('usersController', function($scope) {
+		$scope.users = [
+			{ 
+				"username" : "First-user",
+				"firstName" : "Максим",
+				"lastName" : "Лысаков",
+				"email" : "Lysakov_MS_14@mail.ru",
+				"groups" : [ "group1", "group3", "group4"]
+			},
+			{ 
+				"username" : "BeStUsEr",
+				"firstName" : "Петька",
+				"lastName" : "Васильев",
+				"email" : "nagibator2006@tut.by",
+				"groups" : []
+			},
+			{ 
+				"username" : "Lorem.User",
+				"firstName" : "Ipsim",
+				"lastName" : "Dolor",
+				"email" : "sit-amet@example.com",
+				"groups" : [ "group1", "group2" ]
+			}
+		] // TODO: http get request;
+	});
+
+	app.controller('userController', function($routeParams) {
+		this.username = $routeParams.username;
+	});
+
+	app.controller('groupsController', function($scope) {
+		$scope.groups = [
+			{ 
+				"groupname" : "group1",
+				"users" : [ "First-user", "Lorem.User" ]
+			},
+			{ 
+				"groupname" : "group2",
+				"users" : [ "Lorem.User" ]
+			},
+			{ 
+				"groupname" : "group3",
+				"users" : [ "First-user" ]
+			},
+			{ 
+				"groupname" : "group4",
+				"users" : [ "First-user" ]
+			}
+		] // TODO: http get request;
+	});
+
+	app.controller('groupController', function($routeParams) {
+		this.groupname = $routeParams.groupname;
 	});
 })();
